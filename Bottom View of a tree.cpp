37d2da102655e3423fb3a4b37d5c45298c1eea94
Sanchitgoyal2000 <38776804+Sanchitgoyal2000@users.noreply.github.com>
2019-09-23 09:11:20 +0530

    Add files via upload

---
+++ Bottom View of a tree.cpp
@@ -0,0 +1,35 @@
+void distance(struct Node *root,int *a,int *b,int level,int dist);//acc to distance allocated to each node
+void bottomView(Node *root)
+{
+    int *a=(int *)calloc(100,sizeof(int));
+     int *b=(int *)calloc(100,sizeof(int));
+    for(int i=0;i<100;i++)
+    {
+        b[i]=100;
+        a[i]=-100;  // -100  because we have to take greater values
+    }
+    distance(root,a,b,0,0);
+    for(int i=0;i<100;i++)
+    {
+        if(b[i]!=100)
+        cout<<b[i]<<" ";
+    }
+}
+
+void distance(struct Node *root,int *a,int *b,int level,int dist)
+{
+    if(root==NULL)
+    return;
+    if(dist<0&&level>=a[50+dist])
+    {
+    a[50+dist]=level;
+    b[50+dist]=root->data;
+    }
+    if(dist>=0&&level>=a[50+dist])
+    {
+    a[50+dist]=level;
+    b[50+dist]=root->data;
+    }
+    distance(root->left,a,b,level+1,dist-1);
+    distance(root->right,a,b,level+1,dist+1);
+}
